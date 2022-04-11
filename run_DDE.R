d277112830a6abf9ed8d918f6d8c6eea61a73dc1
Kamil Erguler <kerguler@gmail.com>
2022-04-11 15:02:04 +0300

    revision (draft)

---
+++ run_DDE.R
@@ -0,0 +1,67 @@
+library(stagePop)
+solver.options=list(DDEsolver='PBS',tol=1e-8,hbsize=1e8,dt=0.01)
+
+immigration <- function(mn,std,time) {
+    theta <- std * std / mn
+    k <- mn / theta
+    return(dgamma(time,k,scale=theta))
+}
+
+solveDDE <- function(R1, R2) {
+    init <- 100
+    Tinit <- -40
+    #
+    ccFunctions <- list(
+        deathFunc=function(stage,x,time,species,strain){return(0)},
+        reproFunc=function(x,time,species,strain){return(0)},
+        emigrationFunc=function(stage,x,time,species,strain){return(0)},
+        develFunc=function(stage,x,time,species,strain){ # CONTINUOUS
+            if (time > Tinit) {
+                if (time < 20) {
+                    if (stage == 1) {return(1/R1)} else {return(1e-13)}
+                } else {
+                    if (stage == 1) {return(1/R2)} else {return(1e-13)}
+                }
+            }
+            return(1/R1)
+        },
+        durationFunc=function(stage,x,time,species,strain){ # INITIAL
+            if (time == Tinit) {
+                if (stage == 1) {return(R1)} else {return(1e13)}
+            }
+        },
+        immigrationFunc=function(stage,x,time,species,strain){
+            if (stage==1 && time<=0) {return(v <- immigration(40,5,time-Tinit+20))}
+            return(0)
+        }
+    )
+    #
+    out <- popModel(
+        numSpecies=1,
+        numStages=2,
+        timeDependLoss=FALSE,
+        timeDependDuration=TRUE,
+        ICs=list(matrix(0,nrow=2,ncol=1)),
+        timeVec = seq(Tinit,50,0.01),
+        solverOptions=solver.options,
+        rateFunctions=ccFunctions,
+        stageNames=list(c('juveniles','adults')),
+        speciesNames=c('Culex'),
+        saveFig = FALSE,
+        plotFigs = FALSE
+    )
+    #
+    return(data.frame("time"=out[,1],
+                      "size"=out[,2]))
+}
+
+outS <- solveDDE(40,40) # 20+5
+outM <- solveDDE(60,40)
+outL <- solveDDE(60,60) # 40+5
+
+out <- data.frame("time"=outM$time,
+                  "sizeS"=outS$size,
+                  "sizeM"=outM$size,
+                  "sizeL"=outL$size)
+
+write.csv(out,"mat/solveDDE_out.csv",row.names=FALSE)
--- run_noise.py
+++ run_noise.py
@@ -7,33 +7,39 @@ from spop2 import approximate
 
 prange = [5,50,95]
 
-# Variation in temperature (Gaussian with mean=30 and st.dev.=10)
-def var_temp():
-    return normal(30.0, 10.0)
+# Briere function (https://doi.org/10.1093/ee/28.1.22)
+def briere1(T,T1,T2,a):
+    return a*T*(T-T1)*numpy.sqrt(T2-T)
+
+# Variation in temperature (Gaussian with mean={15,25,35}  and st.dev.=4)
+def var_temp(mu, sd):
+    if sd == 0:
+        return mu
+    y = normal(mu, sd)
+    y = min(50, max(0, y))
+    return y
 
 # Reflection of temperature variation on average development time
-def dev_time(temp, mode):
-    if mode == 'fixed':
-        return 30.0
-    elif mode == 'linear':
-        return numpy.max([1.0, temp]) # Development time of less than 1 unit is not allowed
-    elif mode == 'non-linear':
-        return temp + numpy.abs(temp - 30.0)
+def dev_time(temp):
+    y = briere1(temp, 0, 50, 1.5e-5)
+    if y == 0:
+        return 1e3 # Some very high value
+    return 1.0 / y
 
-# Simulating a population of 100 individuals for 50 steps under deterministic assumption
+# Simulating a population of 100 individuals for 100 steps under deterministic assumption
 # The variation is a result of environmental variation reflected to the daily mean development times
 # Development times are assumed to have a 5 step standard deviation regardless of the mean (Erlang-distributed)
-def sim(mode, eps=1e-4, verbose=False):
+def sim(mu, sd, eps=1e-4, verbose=False):
     approximate(eps)
     a = spop(stochastic=False,gamma_fun="MODE_ACCP_ERLANG")
     a.add(0,0,0,0,100)
     ret = []
     ret.append([0, numpy.nan] + a.read())
-    for i in numpy.arange(50):
-        temp = var_temp()
-        mu = dev_time(temp, mode)
-        a.iterate(0,mu,5,0,0,0)
-        ret.append([i+1,mu]+a.read())
+    for i in numpy.arange(100):
+        temp = var_temp(mu, sd)
+        dev = dev_time(temp)
+        a.iterate(0,dev,5,0,0,0)
+        ret.append([i+1,dev]+a.read())
         if verbose:
             print("Step %d" %(i))
     approximate(0.0)
@@ -48,16 +54,16 @@ def summarize(ret):
         'higher': pandas.DataFrame(tmp[2],columns=cols)
     }
 
-def repeat(mode, nrep, eps=1e-4):
+def repeat(mu, sd, nrep, eps=1e-4):
     ret = []
     counter = 0
     while counter < nrep:
-        ret.append(sim(mode, eps=eps, verbose=False))
+        ret.append(sim(mu, sd, eps=eps, verbose=False))
         counter = len(ret)
         print(counter)
     return ret
 
-def repeatMPI(mode, nrep, eps=1e-4):
+def repeatMPI(mu, sd, nrep, eps=1e-4):
     from mpi4py import MPI
     MPI_SIZE = MPI.COMM_WORLD.Get_size()
     MPI_RANK = MPI.COMM_WORLD.Get_rank()
@@ -71,7 +77,7 @@ def repeatMPI(mode, nrep, eps=1e-4):
         ret = []
         while ret == []:
             try:
-                ret = sim(mode, eps=eps)
+                ret = sim(mu, sd, eps=eps)
             except:
                 pass
         if MPI_RANK==MPI_MASTER:
@@ -88,10 +94,11 @@ def repeatMPI(mode, nrep, eps=1e-4):
             MPI.COMM_WORLD.send(ret, dest=MPI_MASTER, tag=1)
     if MPI_RANK==MPI_MASTER:
         rets = numpy.array(rets)
-        filename = "mat/run_noise_m%s_n%d_e%g.npy" %(mode,nrep,eps)
+        filename = "mat/run_noise_mu%g_sd%g_n%d_e%g.npy" %(mu,sd,nrep,eps)
         print("Saving %s" %(filename))
         numpy.save(filename, rets)
 
 if __name__ == "__main__":
-    # repeatMPI('linear',1000,5e-2)
-    repeatMPI('non-linear',1000,5e-2)
+    repeatMPI(15,4,1000,1e-4)
+    repeatMPI(25,4,1000,1e-4)
+    repeatMPI(35,4,1000,1e-4)
